b9fd0da481650946665229017ce5038b6d91d0f8
Manash Mandal <manashmndl@gmail.com>
2016-02-01 16:30:49 +0600

    files

---
+++ BeachAsif-EEE/BlueSerial-master/app/src/main/java/com/blueserial/ThiefAlert.java
@@ -0,0 +1,31 @@
+package com.blueserial;
+
+/**
+ * Created by Manash on 2/1/2016.
+ */
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.telephony.SmsManager;
+
+public class ThiefAlert  {
+
+    private String phoneNumber;
+    private String message;
+    private SmsManager smsManager;
+
+    //Ctor
+    public ThiefAlert(String phoneNumber, String message){
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+        smsManager = SmsManager.getDefault();
+    }
+
+    public void sendSMS(){
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+    }
+
+
+
+
+}
---
+++ BeachAsif-EEE/README.md
@@ -0,0 +1,16 @@
+# Thief Detector
+
+Will send a sms to the saved number if thief is detected in the house.
+
+## Platform:
+
+* <kbd> Android </kbd>
+* <kbd> Arduino </kbd>
+
+### Components Used:
+* An Android phone
+* PIR Motion Sensor
+* Arduino UNO
+* Bluetooth Module HC-05
+* Mini Breadboard (CLEAR)
+* Jumper Wire
